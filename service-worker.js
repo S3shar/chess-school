5dc660c6ba68ac0601407a8085c75508b490861b
S3shar <david.dzutsev2006@gmail.com>
2025-07-16 13:19:21 +0300

    dfa

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,33 @@
-const CACHE_NAME = 'chess-school';  // Измените версию при обновлении
-const urlsToCache = [
-  '/chess-school/',
-  '/chess-school/index.html',
-  '/chess-school/branches.html',
-  '/chess-school/tournaments.html',
-  '/chess-school/registration.html',
-  '/chess-school/styles.css',
-  '/chess-school/images/main/Logo-Photoroom.png',
-  '/chess-school/images/main/Tel.jpg',
-  '/chess-school/images/main/School1.jpg',
-  '/chess-school/images/main/phon.jpg',
-  '/chess-school/images/icons/web_logo(192).jpg',
-  '/chess-school/images/icons/web_logo(512).jpg'   // Исправленное имя файла
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-      .catch(err => console.log('Cache addAll error:', err))
+    caches.open('dbd-cache').then((cache) => {
+      return cache.addAll([
+        '/index.html',
+        '/features.html',
+        '/characters.html',
+        '/map.html',
+        '/manifest.json',
+        '/style.css',
+        '/images/icons/web_logo(192).jpg',
+        '/images/icons/web_logo(512).jpg',
+        'images/main/inst-logo-Photoroom.png',
+        'images/main/Logo-Photoroom.png',
+        'images/main/phon.jpeg',
+        'images/main/School1.jpg',
+        'images/main/somechess.jpg',
+        'images/main/Tel.jpg',
+        'images/main/telegram-logo-Photoroom.jpg',
+        'images/registration/Phon_register.jpg',
+        'images/registration/Phon-register1.jpg'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
     })
   );
 });
